8323fc8c6e837bce220cd4d04102106567c79e19
wygrajr <wycliffe.graftonjr@gmail.com>
2023-08-13 14:10:37 -0400

    Updated models index.js to not repeat category ID. Added GET ALL code to each route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,7 +5,11 @@ const { Category, Product } = require('../../models');
 
 router.get('/', async (req, res) => {
   try {
-    const categoryData = await Category.findAll();
+    const categoryData = await Category.findAll({
+      include: [
+        { model: Product }
+      ]
+    });
     res.status(200).json(categoryData);
   } catch (err) {
     res.status(500).json(err);
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,7 +4,18 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [
+        { model: Category },
+        { model: Tag }
+      ]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all products
   // be sure to include its associated Category and Tag data
 });
